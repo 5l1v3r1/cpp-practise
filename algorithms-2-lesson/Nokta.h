403bc2252274014c1e126db0e59eed9d86a1f77d
Mert Ozturk <mertdot@icloud.com>
2020-04-20 23:23:40 +0300

    week 4

---
+++ algorithms-2-lesson/Nokta.h
@@ -0,0 +1,12 @@
+//
+// Created by mert on 12.04.2020.
+//
+
+#ifndef CEVAPLAR_NOKTA_H
+#define CEVAPLAR_NOKTA_H
+
+class Nokta{
+
+};
+
+#endif //CEVAPLAR_NOKTA_H
---
+++ algorithms-2-lesson/soru7.cpp
@@ -0,0 +1,10 @@
+//
+// Created by mert on 12.04.2020.
+//
+#include <iostream>
+#include "Dogru.h"
+using namespace std;
+Nokta Dogru::kesisim(Dogru &dogru) {}
+int main(){
+
+}
